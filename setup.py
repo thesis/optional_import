da1e8e5adf65e492ab8cd76959215dba9f9b0ce0
Chris Martin <ch.martin@gmail.com>
2014-06-18 14:31:24 -0400

    convert README md to rst

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     version='1.0',
     py_modules=['optional_import'],
     description='Optional imports in Python',
-    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
     author='Chris Martin',
     author_email='ch.martin@gmail.com',
     url='https://github.com/cardforcoin/optional_import',
